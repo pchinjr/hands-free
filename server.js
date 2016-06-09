321cada6e25daab4ed3c776afd158a868ee57b3c
Paul Chin <pchinjr@gmail.com>
2016-06-09 15:18:49 -0400

    super secure passwords

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 
 var Hapi = require('hapi');
 var Mongoose = require('mongoose');
+var UserStore = require('./lib/userStore');
+
+UserStore.initialize();
 
 var server = new Hapi.Server(); 
 
@@ -14,6 +17,19 @@ Mongoose.connect('mongodb://localhost/handsfree');
 
 server.register(require('inert'), (err) => {
 	if(err) {throw err;}
+});
+
+server.register(require('hapi-auth-cookie'), function(err) {
+	if(err) console.log(err);
+
+	server.auth.strategy('default', 'cookie', {
+		password: 'myPasswordisnotverysecure334555333ZE6#4bM9ot#KEzGPF4ciZXSfgbfJNWZ',
+		cookie: '334555333ZE6#4bM9ot#KEzGPF4ciZXSfgbfJNWZ',
+		redirectTo: '/login',
+		isSecure: false
+	});
+
+	server.auth.default('default');
 });
 
 server.route(require('./lib/routes'));
